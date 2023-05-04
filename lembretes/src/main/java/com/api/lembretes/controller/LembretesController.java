cf88c98f4c00034018085545da8f296a10f56a57
Clara Maestri <75951646+vivalaclara@users.noreply.github.com>
2023-05-04 20:19:39 -0300

    Update LembretesController.java

--- lembretes/src/main/java/com/api/lembretes/controller/LembretesController.java
+++ lembretes/src/main/java/com/api/lembretes/controller/LembretesController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/lembretes")
 public class LembretesController {
     @Autowired
